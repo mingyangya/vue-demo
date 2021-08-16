173901e71bc5790c5e7f8dae1865d551fc809b5e
mingyang.hao <mingyang.hao@geekbang.com>
2021-08-16 19:06:18 +0800

    github配置更新

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,9 @@ function resolve (dir) {
 }
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-demo/'
+    : '/',
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
