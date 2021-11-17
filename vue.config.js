29774d81aa17cd61457e299f9e571770534e71b5
mingyang.hao <mingyang.hao@geekbang.com>
2021-11-17 16:04:39 +0800

    vue.config.js修改

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,11 @@ function resolve (dir) {
 }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-demo/'
-    : '/',
+  publicPath: '/',
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
       .set('@components', resolve("src/components"))
-      .set('gkSrc', resolve('node_modules/@geekbang/geek.ui/src'))
-      .set('gkui', resolve('node_modules/@geekbang/geek.ui/src/components'))
-      .set('gkuiV2', resolve('node_modules/@geekbang/geek.ui/src/v2'))
 
     // 去掉 preload prefetch
     config.plugins.delete('preload')
